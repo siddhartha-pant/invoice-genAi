1d0edf197feb9ebfe0a015d5223ce52484c1d137
siddhartha-pant <iamsiddhartha23@gmail.com>
2026-04-11 11:15:52 +0530

    Clerk functionality added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
+const key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={key}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
